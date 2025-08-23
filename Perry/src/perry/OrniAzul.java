d980bf975a38040a8a37a0f824593a58a5f1a458
Daiana3105 <daianasosacarranza@gmail.com>
2025-08-23 18:14:04 -0300

    Create OrniAzul.java
    nueva clase

---
+++ Perry/src/perry/OrniAzul.java
@@ -0,0 +1,33 @@
+package perry;
+
+import java.util.Scanner;
+
+public class OrniAzul extends papaCastor {
+
+    private double propulsion;
+
+    // Constructor
+    public OrniAzul(Double longitudCola, Double velocidadNado) {
+        super(longitudCola, velocidadNado);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese nivel de propulsión (5 a 10 km/s): ");
+        this.propulsion = sc.nextDouble();
+    }
+
+    // Sobrescribe el método nadar
+    public void nadar() {
+        double total = getNadar() + propulsion;
+        System.out.println("Nada con propulsión a " + total + " km/h.");
+    }
+
+    // Getter para usar en el Comparator
+    public double getPropulsion() {
+        return propulsion;
+    }
+
+    @Override
+    public String toString() {
+        return "OrniAzul{propulsion=" + propulsion + ", velocidadBase=" + getNadar() + "}";
+    }
+}
